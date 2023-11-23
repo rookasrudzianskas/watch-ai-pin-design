eefc91f9bc7d7620bdb69c1bf1cd0e3291de865b
Rokas Rudzianskas <38469892+rookasrudzianskas@users.noreply.github.com>
2023-11-23 16:46:17 +0200

    one liner at all

---
+++ humane Watch App/ContentView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var recordingManager = AudioRecorder()
+
+    var body: some View {
+        VStack {
+            if recordingManager.isRecording {
+                Button("Stop Recording") {
+                    recordingManager.stopRecording()
+                }
+            } else {
+                Button("Start Recording!") {
+                    recordingManager.startRecording()
+                }
+            }
+        }
+    }
+}
---
+++ humane Watch App/humaneApp.swift
@@ -0,0 +1,18 @@
+//
+//  humaneApp.swift
+//  humane Watch App
+//
+//  Created by Rokas on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct humane_Watch_AppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
